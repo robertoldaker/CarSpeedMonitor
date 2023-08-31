7034f07ae9b8bf6a9dcf1de52dbad3e299b1046c
rob oldaker <roldaker@gmail.com>
2023-08-31 11:35:30 +0100

    Put uploading to website in a separate process using a worker nd process queue

--- CarSpeed_upload.py
+++ CarSpeed_upload.py
@@ -8,13 +8,23 @@ import argparse
 import cv2
 import time
 from zipfile import ZipFile
-import requests 
+import requests
+from multiprocessing import Process, Queue
 
 class DetectionUploader:
     def __init__(self):
-        pass
-
+        self.uploadQueue = Queue()
+        self.process = Process(target=DetectionUploader.uploadWorker,args=[self.uploadQueue,])
+        self.process.start()
+    
     def upload(self,result: DetectionResult):
+        self.uploadQueue.put(result)
+    
+    def stop(self):
+        self.uploadQueue.put('STOP')
+    
+    @staticmethod
+    def saveZipfile(result: DetectionResult)->str:
         cap_time = datetime.fromtimestamp(result.posix_time)
         # and save the image to disk
         folder = f'detections/{cap_time.year:04}-{cap_time.month:02}-{cap_time.day:02}'
@@ -39,34 +49,65 @@ class DetectionUploader:
                 myzip.writestr(imageFilename,jpgData)
                 index+=1
 
+        return zipFilename
+
+    @staticmethod
+    def uploadZipfile(fn):
         # upload to website
-        with open(zipFilename, 'rb') as f:
-            files = {"file": f}            
-            r = requests.post("http://ser5.local:5174/Detections/Upload", files=files) 
-            print(r.status_code)
-            print(r.text)
+        uploaded = False
+        with open(fn, 'rb') as f:
+            files = {"file": f}
+            try:
+                r = requests.post("http://odin.local:5030/Detections/Upload", files=files)
+                if r.status_code==200:
+                    uploaded = True
+                else:
+                    print(f"Invalid ststus code uploaded detection [{r.status_code}]");
+            except:
+                print(f"Problem uploading detection")                
+        if uploaded:
+            os.remove(fn)
+        
 
 
-def car_detected(data: DetectionResult):
-    st:float = time.monotonic()
-    detectionUploader.upload(data)
-    ft=time.monotonic()
-    print(f"uploaded in {(ft-st):6.3f}s")
+
+    @staticmethod
+    def _uploadWorker(q: Queue):
+        for fn in iter(q.get, 'STOP'):
+            print(fn)
+
+    @staticmethod
+    def uploadWorker(q: Queue):
+        for dr in iter(q.get, 'STOP'):
+            st:float = time.monotonic()
+            fn = DetectionUploader.saveZipfile(dr)
+            DetectionUploader.uploadZipfile(fn)
+            ft=time.monotonic()
+            print(f"Uploaded detection in {(ft-st):6.3f}s")
+        print('Upload queue stopped')
 
 
-ap = argparse.ArgumentParser(description="Monitors car speed using raspberry pi camera")
-ap.add_argument("--file","-f", default=CarSpeedConfig.DEF_CONFIG_FILE, help="Filename to store config")
-ap.add_argument("--preview","-p", action='store_true', help="Create preview window")
-args = vars(ap.parse_args())
+def main():
+    def car_detected(result: DetectionResult):
+        detectionUploader.upload(result)
+    ap = argparse.ArgumentParser(description="Monitors car speed using raspberry pi camera")
+    ap.add_argument("--file","-f", default=CarSpeedConfig.DEF_CONFIG_FILE, help="Filename to store config")
+    ap.add_argument("--preview","-p", action='store_true', help="Create preview window")
+    args = vars(ap.parse_args())
 
-show_preview = args["preview"]
+    show_preview = args["preview"]
 
-detectionUploader = DetectionUploader()
+    detectionUploader = DetectionUploader()
 
-# open config
-config = CarSpeedConfig.fromDefJsonFile()
+    # open config
+    config = CarSpeedConfig.fromDefJsonFile()
 
-# start the monitor
-if config!=None:
-    proc = CarSpeedMonitor(config)
-    proc.start(detection_hook=car_detected,show_preview=show_preview)
+    # start the monitor
+    if config!=None:
+        proc = CarSpeedMonitor(config)
+        proc.start(detection_hook=car_detected,show_preview=show_preview)
+        detectionUploader.stop()
+        print('detection uploader stopped')
+
+if __name__ == '__main__':
+    main()
