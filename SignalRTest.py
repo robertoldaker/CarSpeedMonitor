06a724b4deb65326957d966692b2d5646017b453
rob oldaker <roldaker@gmail.com>
2023-10-27 13:16:30 +0100

    used propert SignalRHandler in test script

--- SignalRTest.py
+++ SignalRTest.py
@@ -1,73 +1,91 @@
 import logging
+import sys
+import threading
 from typing import Callable
+import requests
 from signalrcore.hub_connection_builder import HubConnectionBuilder
 from signalrcore.protocol.messagepack_protocol import MessagePackHubProtocol
 import platform
+from CarSpeedConfig import CarSpeedConfig
 
-class SignalRHandler:
-    def __init__(self, name):
-        #        .configure_logging(logging.DEBUG)\
-        server_url = "http://localhost:5174/NotificationHub"
-        self.hub_connection = HubConnectionBuilder()\
-        .with_url(server_url, options={
-            "headers": {"monitor-name": name}
-        })\
-        .with_hub_protocol(MessagePackHubProtocol())\
-        .with_automatic_reconnect({
-            "type": "raw",
-            "keep_alive_interval": 10,
-            "reconnect_interval": 5,
-            "max_attempts": 5
-        }).build()
+from SignalRHandler import SignalRHandler
 
-        self.hub_connection.on_open(lambda: print("connection opened and handshake received ready to send messages"))
-        self.hub_connection.on_close(lambda: print("connection closed"))
-
-        self.hub_connection.on("NewDetectionLoaded", SignalRHandler.newDetectionLoaded)
-    
-    def start(self):
-        self.hub_connection.start()
-    
-    def stop(self):
-        self.hub_connection.stop()
-    
-    def uploadPreview(self,image):
-        #send_callback_received = threading.Lock()
-        #send_callback_received.acquire()
-        #image="hello!"
-        #image = bytearray()
-        #image.append(0xA2)
-        #image.append(0x01)
-        #image.append(0x02)
-        #image.append(0x03)
-        #image.append(0x04)
-        image = memoryview(b'still allows embedded "double" quotes')
-        self.hub_connection.send(
-            "PreviewImage", # Method
-            [image], # Params
-        )
-            #lambda m: send_callback_received.release()) # Callback
-        #if not send_callback_received.acquire(timeout=1):
-        #    raise ValueError("CALLBACK NOT RECEIVED")
-
-    @staticmethod
-    def newDetectionLoaded(args):
-        print("New detection loaded!!")
-
-
-def messageReceived(args):
-    print("message received!!!")
 
 def main():
-    computerName = platform.node()
-    print(f'name={computerName}')
+    def startMonitor(args):
+        print('start monitor')
+
+    def stopMonitor(args):
+        print('stop monitor')
+
+    def toggleDetection(args):
+        print('toggle detection')
+
+    def resetTracking(args):
+        print('reset tracking')
+    
+    def shutdown(args):
+        print('shutdown')
+        exitEvent.set()
+
+    def reboot(args):
+        print('reboot')
+        exitEvent.set()
+
+    def configEdited(args):
+        print('config edited')
+
+    def onConnection(connected: bool):
+        if connected:
+            print('on connection')
+            hubConnectionEvent.set()
+
+    def getConfig()->CarSpeedConfig|None:        
+        try:
+            # load config from db
+            config = loadConfig()
+            return config
+        except Exception as e:
+            print(f'Error loading config [{e.args[0]}]')
+            signalRHandler.logMessage(e.args[0])
+
+    def loadConfig()->CarSpeedConfig:
+        r = requests.get(f"{rootUrl}/Monitor/Config",params={'monitorName': monitorName})
+        if r.status_code==200:
+            newConfig = CarSpeedConfig.fromJsonStr(r.text)
+            if not newConfig:
+                raise Exception("Could not parse config")
+        else:
+            raise Exception(f"Invalid status code downloading new config [{r.status_code}]")
+        return newConfig
+
+    rootUrl = 'http://localhost:5174'
+    hubConnectionEvent = threading.Event()
+    exitEvent = threading.Event()
+    monitorName = platform.node()
+    print(f'name={monitorName}')    
     key = None
-    sigr = SignalRHandler(computerName)
-    sigr.start()
-    while key != "e":        
-        key = input(">> ")
-        if key == "u":
-            sigr.uploadPreview(1)
+    signalRHandler = SignalRHandler(rootUrl,monitorName)
+    signalRHandler.onConnection = onConnection
+    signalRHandler.hub_connection.on("StartMonitor", startMonitor)
+    signalRHandler.hub_connection.on("StopMonitor", stopMonitor)
+    signalRHandler.hub_connection.on("ToggleDetection", toggleDetection)
+    signalRHandler.hub_connection.on("ResetTracking", resetTracking)
+    signalRHandler.hub_connection.on("Shutdown", shutdown)
+    signalRHandler.hub_connection.on("Reboot", reboot)
+    signalRHandler.hub_connection.on("MonitorConfigEdited", configEdited)
+
+    signalRHandler.start()
+
+    print('Waiting for hub connection ...')
+    config = getConfig()
+    if config:
+        print(f'configId={config.id}')
+
+    print('Waiting for exit ...')
+    exitEvent.wait()
+    
+    signalRHandler.stop()
 
 if __name__ == '__main__':
     main()
